6567e203505ef67174e47fd4b95df7fa14619b62
MilosS019 <milosstanojlovic019@gmail.com>
2022-12-17 04:37:04 +0100

    Added another constructor for the vehicle, created VehicleTypeController and created another constructor for Location

---
+++ tim7iss/src/main/java/com/tim7/iss/tim7iss/controllers/VehicleTypeController.java
@@ -0,0 +1,29 @@
+package com.tim7.iss.tim7iss.controllers;
+
+import com.tim7.iss.tim7iss.models.VehicleType;
+import com.tim7.iss.tim7iss.services.VehicleTypeService;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/vehicleType")
+@CrossOrigin
+@Transactional
+public class VehicleTypeController {
+    @Autowired
+    VehicleTypeService vehicleTypeService;
+
+    @GetMapping
+    public ResponseEntity<List<VehicleType>>findAll(){
+        List<VehicleType> vehicleTypes = vehicleTypeService.findAll();
+        return new ResponseEntity<>(vehicleTypes, HttpStatus.OK);
+    }
+}
